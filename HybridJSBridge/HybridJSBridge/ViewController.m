444f85896788b6b5ee632a33c5654de71904d7a6
wanghuizhou <wanghuizhou@guazi.com>
2019-12-16 14:15:35 +0800

    ### 12

--- HybridJSBridge/HybridJSBridge/ViewController.m
+++ HybridJSBridge/HybridJSBridge/ViewController.m
@@ -110,6 +110,7 @@ typedef void (^WWJBHandler)(id data, WWJBResponseCallback responseCallback);
     
     
     // 11:
+    // 12:
     
 //    WWJBResponseCallback wwResponseCallBack = ^(NSString * data){
 //        NSLog(@"-----执行了wwResponseCallBack");
